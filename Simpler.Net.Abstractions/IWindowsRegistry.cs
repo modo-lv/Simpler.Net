e2ef1693c19ae850196d4379bd1d160f98c3fc6f
modo-lv <martins.osis@gmail.com>
2016-02-09 22:43:39 +0200

    Added Simpler.Net.Abstractions projects. Added Windows Registry abstraction class and interface.

---
+++ Simpler.Net.Abstractions/IWindowsRegistry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Simpler.Net.Abstractions
+{
+	/// <summary>
+	/// Abstraction interface for <see cref="Registry"/>.
+	/// </summary>
+	public interface IWindowsRegistry
+	{
+		/// <summary>
+		/// See <see cref="Registry.ClassesRoot"/>.
+		/// </summary>
+		RegistryKey ClassesRoot { get; }
+		/// <summary>
+		/// See <see cref="Registry.CurrentConfig"/>.
+		/// </summary>
+		RegistryKey CurrentConfig { get; }
+		/// <summary>
+		/// See <see cref="Registry.CurrentUser"/>.
+		/// </summary>
+		RegistryKey CurrentUser { get; }
+		/// <summary>
+		/// See <see cref="Registry.DynData"/>.
+		/// </summary>
+		RegistryKey DynData { get; }
+		/// <summary>
+		/// See <see cref="Registry.PerformanceData"/>.
+		/// </summary>
+		RegistryKey PerformanceData { get; }
+		/// <summary>
+		/// See <see cref="Registry.Users"/>.
+		/// </summary>
+		RegistryKey Users { get; }
+
+		/// <summary>
+		/// See <see cref="Registry.GetValue"/>.
+		/// </summary>
+		Object GetValue(String keyName, String valueName, Object defaultValue);
+
+		/// <summary>
+		/// See <see cref="Registry.SetValue(string,string,object)"/>.
+		/// </summary>
+		void SetValue(String keyName, String valueName, Object value);
+
+		/// <summary>
+		/// See <see cref="Registry.SetValue(string,string,object,RegistryValueKind)"/>.
+		/// </summary>
+		void SetValue(String keyName, String valueName, Object value, RegistryValueKind valueKind);
+	}
+}
---
+++ Simpler.Net.Abstractions/WindowsRegistry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Simpler.Net.Abstractions
+{
+	/// <summary>
+	/// Concrete implementation for <see cref="IWindowsRegistry"/>, forwarding calls to
+	/// to static <see cref="Registry"/> fields and methods.
+	/// </summary>
+	public class WindowsRegistry : IWindowsRegistry
+	{
+		public RegistryKey ClassesRoot => Registry.ClassesRoot;
+
+		public RegistryKey CurrentConfig => Registry.CurrentConfig;
+
+		public RegistryKey CurrentUser => Registry.CurrentUser;
+
+		[ObsoleteAttribute("The DynData registry key only works on Win9x, which is no longer supported by the CLR.  On NT-based operating systems, use the PerformanceData registry key instead.")]
+		public RegistryKey DynData => Registry.DynData;
+
+		public RegistryKey PerformanceData => Registry.PerformanceData;
+
+		public RegistryKey Users => Registry.Users;
+
+
+		public Object GetValue(String keyName, String valueName, Object defaultValue)
+		{
+			return Registry.GetValue(keyName, valueName, defaultValue);
+		}
+
+
+		public void SetValue(String keyName, String valueName, Object value)
+		{
+			Registry.SetValue(keyName, valueName, value);
+		}
+
+
+		public void SetValue(String keyName, String valueName, Object value, RegistryValueKind valueKind)
+		{
+			Registry.SetValue(keyName, valueName, value, valueKind);
+		}
+	}
+}
