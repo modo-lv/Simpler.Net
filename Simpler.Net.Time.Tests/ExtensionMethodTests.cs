eb1ac932b021031c2ba6a12124fc189f1132372e
modo-lv <martins.osis@gmail.com>
2016-03-13 18:50:03 +0200

    Added Simpler.Net.Time project.

---
+++ Simpler.Net.Time.Tests/ExtensionMethodTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simpler.Net.Time.Tests
+{
+	[TestClass]
+	public class ExtensionMethodTests
+	{
+		[TestMethod]
+		public void ToUnixTimestampInt32_ConvertsCorrectly()
+		{
+			// ARRANGE
+			var dt = new DateTime(2016, 01, 01, 01, 01, 01);
+			
+			// ACT
+			var result = dt.ToUnixTimestampInt32();
+
+			// ASSERT
+			const Int32 expect = 1451610061;
+			Assert.AreEqual(expect, result);
+		}
+	}
+}
---
+++ Simpler.Net.Time/TimeExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Simpler.Net.Time
+{
+	public static class TimeExtensionMethods
+	{
+		/// <summary>
+		/// Get the UNIX timestamp of a given time.
+		/// </summary>
+		/// <param name="time">Time to convert to UNIX timestamp.</param>
+		/// <returns></returns>
+		public static Int32 ToUnixTimestampInt32(this DateTime time)
+		{
+			var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+			return Convert.ToInt32(Math.Floor((time - origin).TotalSeconds));
+		}
+	}
+}
