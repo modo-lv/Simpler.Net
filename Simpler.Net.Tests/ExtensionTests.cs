53d236f6f4ea8b58802ac815eafe0e5515021289
modo-lv <martins.osis@gmail.com>
2016-03-29 14:30:47 +0300

    [Simpler.Net] Added IfNotNull() extension method. Changed framework to 3.5 to use extension methods and Func<>. Changed version number.

---
+++ Simpler.Net.Tests/ExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simpler.Net.Tests
+{
+	[TestClass]
+	public class ExtensionTests
+	{
+		[TestMethod]
+		public void Extension_IfNotNull_CallsMethodIfNotNull()
+		{
+			// ARRANGE
+			var obj = new Object();
+			var expect = obj.ToString();
+
+			// ACT
+			var result = obj.IfNotNull(o => o.ToString());
+
+			// ASSERT
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Extension_IfNotNull_ReturnsFallbackValueIfNull()
+		{
+			// ARRANGE
+			Object obj = null;
+			const String expect = "Fallback value";
+
+			// ACT
+			// ReSharper disable once ExpressionIsAlwaysNull
+			var result = obj.IfNotNull(o => o.ToString(), otherwise:expect);
+
+			// ASSERT
+			Assert.AreEqual(expect, result);
+		}
+	}
+}
---
+++ Simpler.Net/SimplerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Simpler.Net
+{
+	/// <summary>
+	/// Miscellaneous helpful extension methods.
+	/// </summary>
+	public static class SimplerExtensions
+	{
+		/// <summary>
+		/// Execute a function with an object if that object is not null.
+		/// </summary>
+		/// <param name="obj">Object to exectue the function on.</param>
+		/// <param name="func">Fuction to execute on the object.</param>
+		/// <param name="otherwise">Default value to return if the object is null.</param>
+		/// <typeparam name="TObject">Type of object to check.</typeparam>
+		/// <typeparam name="TResult">Type of the function result / default value.</typeparam>
+		/// <returns>Result of function if object was not null, otherwise the default value of TResult.</returns>
+		public static TResult IfNotNull<TObject, TResult>(this TObject obj,
+			Func<TObject, TResult> func,
+			TResult otherwise = default(TResult))
+		{
+			return obj != null ? func(obj) : otherwise;
+		}
+	}
+}
