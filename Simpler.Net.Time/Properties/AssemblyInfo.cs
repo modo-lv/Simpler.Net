cdbd93de1a28eb5121a9797a827f99d4e703b91d
modo-lv <modo@modo.lv>
2017-02-24 01:31:42 +0200

    Added Int64 version of the UNIX timestamp. Added .NET Standard version.

--- Simpler.Net.Time/Properties/AssemblyInfo.cs
+++ Simpler.Net.Time/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿#if !NETSTANDARD1_0
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simpler.Net.Time")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Simpler.Net.Time")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.2")]
-[assembly: AssemblyFileVersion("0.0.1.2")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+#endif
--- Simpler.Net.Time/TimeExtensionMethods.cs
+++ Simpler.Net.Time/TimeExtensionMethods.cs
@@ -2,6 +2,9 @@
 
 namespace Simpler.Net.Time
 {
+	/// <summary>
+	/// Miscellaneous methods to help working with dates and times.
+	/// </summary>
 	public static class TimeExtensionMethods
 	{
 		/// <summary>
@@ -11,8 +14,19 @@ namespace Simpler.Net.Time
 		/// <returns></returns>
 		public static Int32 ToUnixTimestampInt32(this DateTime time)
 		{
-			var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+			var origin = new DateTime(year: 1970, month: 1, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0);
 			return Convert.ToInt32(Math.Floor((time - origin).TotalSeconds));
+		}
+
+		/// <summary>
+		/// Get the UNIX timestamp (in seconds) of a given time.
+		/// </summary>
+		/// <param name="time">Time to convert to UNIX timestamp.</param>
+		/// <returns>Number of seconds since UNIX epoch (1970-01-01 00:00:00)</returns>
+		public static Int64 ToUnixTimestampInt64(this DateTime time)
+		{
+			var origin = new DateTime(year: 1970, month: 1, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0);
+			return Convert.ToInt64(Math.Floor((time - origin).TotalSeconds));
 		}
 	}
 }
