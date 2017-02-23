7a6dfd20a212fdedf4cd33662d47e9fcbac9196c
modo-lv <modo@modo.lv>
2017-02-24 00:57:54 +0200

    Added HTTP project. Added MIME type constants.

---
+++ Simpler.Net.Http/ShortMimeTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Simpler.Net.Http
+{
+	/// <summary>
+	/// Shorthands for commonly used MIME types.
+	/// </summary>
+	public static class ShortMimeTypes
+	{
+		public const String Binary = "application/octet-stream";
+		public const String Jpeg = "image/jpeg";
+		public const String Json = "application/json";
+		public const String Gif = "image/gif";
+		public const String Html = "text/html";
+		public const String Pdf = "application/pdf";
+		public const String Png = "image/png";
+		public const String Text = "text/plain";
+		public const String Xml = "application/xml";
+		public const String Zip = "application/zip";
+	}
+}
