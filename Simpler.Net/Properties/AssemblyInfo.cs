04b79cdede0cb65f7b557575b60bb18022e0ad0b
modo-lv <modo@modo.lv>
2017-06-29 12:15:46 +0300

    Removed IfNotNull() extension method.

--- Simpler.Net/Properties/AssemblyInfo.cs
+++ Simpler.Net/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿#if !NETSTANDARD
+﻿#if !NETSTANDARD1_0
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
--- Simpler.Net/SimplerExtensions.cs
+++ Simpler.Net/SimplerExtensions.cs
@@ -9,24 +9,6 @@ namespace Simpler.Net
 	public static class SimplerExtensions
 	{
 		/// <summary>
-		/// Execute a function with an object if that object is not null.
-		/// </summary>
-		/// <param name="obj">Object to exectue the function on.</param>
-		/// <param name="func">Fuction to execute on the object.</param>
-		/// <param name="otherwise">Default value to return if the object is null.</param>
-		/// <typeparam name="TObject">Type of object to check.</typeparam>
-		/// <typeparam name="TResult">Type of the function result / default value.</typeparam>
-		/// <returns>Result of function if object was not null, otherwise the default value of TResult.</returns>
-		public static TResult IfNotNull<TObject, TResult>(
-			this TObject obj,
-			Func<TObject, TResult> func,
-			TResult otherwise = default(TResult))
-		{
-			return obj != null ? func(obj) : otherwise;
-		}
-
-
-		/// <summary>
 		/// Syntactic sugar wrapping around <see cref="IDictionary{TKey,TValue}"/> accessor
 		/// allowing to provide a fallback value in case the supplied key is not found in the
 		/// dictionary.
