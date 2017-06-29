dd28a78a2dc2c367c187bb734b29144cebd77caf
modo-lv <modo@modo.lv>
2017-06-29 12:05:56 +0300

    Removed SimplerTreeNode, updated ReadMe.

--- Simpler.Net/README.md
+++ Simpler.Net/README.md
@@ -1,9 +1,18 @@
-Simpler.Net
-===========
-
-All sorts of miscellaneous, mostly atomic utilities, methods, workarounds and wrappers.
+# Simpler.Net
 
 
-SimplerTreeNode
----------------
-A very basic, simple class for storing data in a tree structure.
+Miscellaneous, mostly atomic utilities, methods, workarounds and wrappers.
+
+## Extension methods
+
+### Dictionary<>.Get()
+A concise and readable way to retrieve a dictionary entry if it exists, or a default value if it doesn't.
+
+```cs
+var foo = new Dictionary<String, Int>();
+Console.WriteLine(foo.Get("none"));
+Console.WriteLine(foo.Get("none", fallback:12));
+// Output:
+// 		0
+// 		12
+```
--- Simpler.Net/SimplerExtensions.cs
+++ Simpler.Net/SimplerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Simpler.Net
 {
@@ -16,11 +17,34 @@ namespace Simpler.Net
 		/// <typeparam name="TObject">Type of object to check.</typeparam>
 		/// <typeparam name="TResult">Type of the function result / default value.</typeparam>
 		/// <returns>Result of function if object was not null, otherwise the default value of TResult.</returns>
-		public static TResult IfNotNull<TObject, TResult>(this TObject obj,
+		public static TResult IfNotNull<TObject, TResult>(
+			this TObject obj,
 			Func<TObject, TResult> func,
 			TResult otherwise = default(TResult))
 		{
 			return obj != null ? func(obj) : otherwise;
+		}
+
+
+		/// <summary>
+		/// Syntactic sugar wrapping around <see cref="IDictionary{TKey,TValue}"/> accessor
+		/// allowing to provide a fallback value in case the supplied key is not found in the
+		/// dictionary.
+		/// </summary>
+		/// <typeparam name="TKey">Dictionary key type.</typeparam>
+		/// <typeparam name="TValue">Dictionary value type.</typeparam>
+		/// <param name="dictionary">Dictionary to get value from.</param>
+		/// <param name="key">Key for finding the value.</param>
+		/// <param name="fallback">Value to return if the key is not found.</param>
+		/// <returns>Found value or <paramref name="fallback"/>.</returns>
+		public static TValue Get<TKey, TValue>(
+			this IDictionary<TKey, TValue> dictionary,
+			TKey key,
+			TValue fallback = default(TValue))
+		{
+			if (dictionary.ContainsKey(key))
+				return dictionary[key];
+			return fallback;
 		}
 	}
 }
