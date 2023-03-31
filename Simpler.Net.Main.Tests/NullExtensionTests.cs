75915d746332a8dc9f4587b800ea0a352817327a
modo_lv <modo@modo.lv>
2023-03-31 19:54:30 +0300

    Added `ToStringOr` and `ToStringOrEmpty` extensions

---
+++ Simpler.Net.Main.Tests/NullExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Simpler.Net.Main;
+
+namespace Simpler.Net.Tests;
+
+public class NullExtensionTests {
+  [TestCase(null, "fallback", TestName = "Null input produces fallback", ExpectedResult = "fallback")]
+  [TestCase(123, "321", TestName = "Non-null input produces input as string", ExpectedResult = "123")]
+  public String ToStringOr(Object? input, String fallback) {
+    return input.ToStringOr(fallback);
+  }
+
+  [TestCase(null, TestName = "Null input produces empty string", ExpectedResult = "")]
+  [TestCase(123, TestName = "Non-null input produces input as string", ExpectedResult = "123")]
+  public String ToStringOrEmpty(Object? input) {
+    return input.ToStringOrEmpty();
+  }
+
+}
---
+++ Simpler.Net.Main/NullExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Simpler.Net.Main;
+
+public static class NullExtensions {
+  /// <summary>
+  /// Return a value's <see cref="String"/> representation, or a fallback if the value is null.
+  /// A fluent alternative to <code>value?.ToString() ?? fallback;</code>
+  /// </summary>
+  /// <param name="value">Value to call <c>.ToString()</c> on.</param>
+  /// <param name="fallback">Fallback value to return if <paramref name="value"/> is <c>null</c>.</param>
+  /// <returns>String representation of <paramref name="value"/> or <paramref name="fallback"/>.</returns>
+  public static String ToStringOr(this Object? value, String fallback = "") {
+    return value?.ToString() ?? fallback;
+  }
+
+  /// <summary>
+  /// Return a value's <see cref="String"/> representation, or an empty string if the value is null.
+  /// A fluent alternative to <code>value?.ToString() ?? String.Empty;</code>
+  /// </summary>
+  /// <param name="value">Value to call <c>.ToString()</c> on.</param>
+  /// <returns>String representation of <paramref name="value"/> or <see cref="string.Empty"/>.</returns>
+  public static String ToStringOrEmpty(this Object? value) =>
+    ToStringOr(value, String.Empty);
+}
